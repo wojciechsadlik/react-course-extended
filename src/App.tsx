fb0133dbbc3c6d6b1f08534c34444a010f21ec4e
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-01-18 22:41:54 +0100

    printing variables experiments

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import './App.css';
 
 function App() {
+  const title = "Messing around with different data";
+  const num = 50;
+  const arr = [1, 2, 3];
+  const rand = Math.random() * 10;
+  const link = "https://www.google.com/";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Number: {num}</p>
+        <p>Numbers: {arr}</p>
+        <p>Random number: {rand}</p>
+
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
