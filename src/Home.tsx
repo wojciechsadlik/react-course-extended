4a6873844a459a554292ec92f4a7a63c4a6c19f1
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-01-19 11:28:39 +0100

    using useState

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,21 +1,29 @@
-import React, { SyntheticEvent } from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 
 const Home = () => {
+  const [name, setName] = useState("Name");
+  const [clicks, setClicks] = useState(0);
+
   const handleClick = (e: SyntheticEvent) => {
-    console.log("handleClick is working", e);
+    setName("Other Name");
   };
 
-  const handleClick2 = (name: string, e: SyntheticEvent) => {
-    console.log("handleClick " + name, e);
+  const incrementClicks = (e: SyntheticEvent) => {
+    setClicks(clicks + 1);
+  };
+
+  const decrementClicks = (e: SyntheticEvent) => {
+    setClicks(clicks - 1);
   };
 
   return (
     <div className="home">
         <h2>Homepage</h2>
         <button onClick={handleClick}>Click me</button>
-        <button onClick={(e) => {
-          handleClick2("Name", e);
-        }}>Click me 2</button>
+        <p>{name}</p>
+        <button onClick={incrementClicks}>Increment Clicks</button>
+        <button onClick={decrementClicks}>Decrement Clicks</button>
+        <p>{clicks}</p>
     </div>
   );
 }
