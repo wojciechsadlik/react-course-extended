209e6973961ae35a0ca3ad761ab4a7c6fa595729
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-01-19 11:17:18 +0100

    handling button clicks

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 
 const Home = () => {
+  const handleClick = (e: SyntheticEvent) => {
+    console.log("handleClick is working", e);
+  };
+
+  const handleClick2 = (name: string, e: SyntheticEvent) => {
+    console.log("handleClick " + name, e);
+  };
+
   return (
     <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => {
+          handleClick2("Name", e);
+        }}>Click me 2</button>
     </div>
   );
 }
