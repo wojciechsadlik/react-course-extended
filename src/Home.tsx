705a3988546ea26c13c50e813eb5234d06c0c22c
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-01-19 21:11:20 +0100

    fetch error message

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,24 +3,36 @@ import BlogList from './BlogList';
 import IBlogEntry from './IBlogEntry';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(Array<IBlogEntry>());
+  const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch blogs");
+        }
+        
         return res.json();
       })
       .then((data) => {
         setIsPending(false);
+        setError(null);
         setBlogs(data);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
+        console.log(err.message);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs.length !== 0 && <BlogList blogs={blogs} title="All Blogs"/>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   );
 }
