280c0c100041c0c749c3cb7b06f70c0b2b880bc1
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-01-20 22:31:25 +0100

    routing blog previews to blog details

--- src/BlogList.tsx
+++ src/BlogList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import IBlogEntry from './IBlogEntry';
 
 interface BlogListProps {
@@ -12,8 +13,10 @@ const BlogList = ({ blogs, title }: BlogListProps) => {
       <h2>{ title }</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
         </div>
       ))}
     </div>
