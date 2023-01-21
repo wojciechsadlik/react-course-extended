a3ee6748b302186511e064e1983f8348891229be
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-01-21 14:50:30 +0100

    create blog input form, formatting moved to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
--- src/Create.tsx
+++ src/Create.tsx
@@ -4,6 +4,22 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+        />
+        <label>Blog body:</label>
+        <textarea required></textarea>
+        <label>Blog author:</label>
+        <select>
+          <option value="author1">author1</option>
+          <option value="author2">author2</option>
+          <option value="author3">author3</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 };
