72a31839f561157240c44de8145adc5069d39e3e
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-01-22 21:16:08 +0100

    express configuration

---
+++ backend/src/App.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+    res.send("Express is working");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: App backend is listening on port ${port}`);
+});
